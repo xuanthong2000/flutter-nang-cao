5aa0391a1316571ab5cdbefacd8890b8bd706edc
xuanthong2000 <56944271+xuanthong2000@users.noreply.github.com>
2021-12-26 22:50:31 +0700

    dang nhap dang ky

--- newapp/lib/main.dart
+++ newapp/lib/main.dart
@@ -1,11 +1,17 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:flutter/cupertino.dart';
+import 'firebase.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  MobileAds.instance.initialize();
   runApp(MyApp());
-  
+  await Firebase.initializeApp();
+
 }
 
 class MyApp extends StatelessWidget {
