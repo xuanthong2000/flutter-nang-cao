ab85b4da2f909faddba15fedc7a01e295bfcdeb7
xuanthong2000 <56944271+xuanthong2000@users.noreply.github.com>
2021-12-27 09:33:55 +0700

    dang lam form

---
+++ newapp/lib/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  final User user;
+  const HomePage({Key key, this.user}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+}
