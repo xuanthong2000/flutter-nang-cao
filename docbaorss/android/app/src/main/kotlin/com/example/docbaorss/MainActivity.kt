75f9141002c0f2297af663e656ca4d846f233b54
xuanthong2000 <56944271+xuanthong2000@users.noreply.github.com>
2021-12-28 10:16:09 +0700

    t

--- docbaorss/android/app/src/main/kotlin/com/example/docbaorss/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.docbaorss
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- docbaorss/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:docbaorss/src/app.dart';
-import 'package:flutter/material.dart';
-
-void main() async {
-  await DocBaoApp.initiate();
-  runApp(DocBaoApp());
-}
